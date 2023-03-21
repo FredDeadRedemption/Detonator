5e59818c0fb71ab20f9b4dc6add9691fd586a893
Fred_Dead_Redemption <frederikdau@gmail.com>
2023-03-21 09:31:14 +0100

    init

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//yeehaw
-console.log("yeehaw");
+"use strict";
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+canvas.middle = canvas.width / 2; //y axis middle
+
+c.fillRect(0, 0, canvas.width, canvas.width);
+
+//test
+c.fillStyle = "red";
+c.fillRect(50, 50, 50, 50);
