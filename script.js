870842a33962ec5034af47f5309e45e36b4c0929
Fred_Dead_Redemption <frederikdau@gmail.com>
2023-03-21 11:05:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 "use strict";
+//canvas init
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
@@ -8,6 +9,6 @@ canvas.middle = canvas.width / 2; //y axis middle
 
 c.fillRect(0, 0, canvas.width, canvas.width);
 
-//test
+//test square
 c.fillStyle = "red";
 c.fillRect(50, 50, 50, 50);
