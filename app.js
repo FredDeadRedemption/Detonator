7d35f57ff5a8ecdae15c069652cc3fe43868233c
Fred_Dead_Redemption <frederikdau@gmail.com>
2023-03-30 12:32:01 +0200

    server init
    kør app med "npm run dev" i powershell eller bash

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require("express");
 
 const app = express();
 
-const server = require("http").createServer(app);
+const http = require("http");
 
-const port = 4200;
+const server = http.createServer(app);
+
+const port = 420;
+
+const { Server } = require("socket.io");
+
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 app.use("/client", express.static(__dirname + "/client"));
+
+io.on("connection", (socket) => {
+  console.log("user connected: " + socket.id);
+});
 
 server.listen(port, (error) => {
   if (error) {
@@ -17,10 +27,4 @@ server.listen(port, (error) => {
   } else {
     console.log("server running on port: " + port);
   }
-});
-
-const io = require("socket.io")(server, {});
-
-io.on("connection", (socket) => {
-  console.log("user connected: " + socket.id);
 });
