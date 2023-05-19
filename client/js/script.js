e80004396891332df688c6cefa44bdb418fe7bd2
Fred_Dead_Redemption <frederikdau@gmail.com>
2023-05-19 23:13:36 +0200

    fullscreen mode

--- client/js/script.js
+++ client/js/script.js
@@ -69,6 +69,9 @@ window.addEventListener("keydown", (event) => {
           ability();
         }
         break;
+      case "Escape":
+        canvas.requestFullscreen();
+        break;
     }
   }
 });
